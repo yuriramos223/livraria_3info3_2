e43ab28ae1fb98a823e94ffe4f304a35f33f8d15
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2026-03-17 16:31:19 -0300

    Feat: inclusão do relacionamento entre livro e autor

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from .autor import Autor
 from .categoria import Categoria
 from .editora import Editora
 
@@ -11,6 +12,7 @@ class Livro(models.Model):
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
+    autores = models.ManyToManyField(Autor, related_name='livros')
 
     def __str__(self):
         return f'({self.id}) {self.titulo} ({self.quantidade})'
