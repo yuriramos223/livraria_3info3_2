21c0f2fe839105c4814a1fc7de8612308cd7ad03
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2026-03-17 16:08:07 -0300

    Feat: inclusão do relacionamento entre livro e editora

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from .categoria import Categoria
+from .editora import Editora
 
 
 class Livro(models.Model):
@@ -9,6 +10,7 @@ class Livro(models.Model):
     quantidade = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
 
     def __str__(self):
         return f'({self.id}) {self.titulo} ({self.quantidade})'
