ec640bed5c6d40747c6ca70360afaa99f8ac6a2c
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2026-03-17 09:27:39 -0300

    Feat: ligação entre tabelas

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+from .categoria import Categoria
+
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
 
     def __str__(self):
         return f'({self.id}) {self.titulo} ({self.quantidade})'
