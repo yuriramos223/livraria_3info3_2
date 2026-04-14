85855a8088141b6caf8e3325b6c418dfba01ae50
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2026-04-14 16:03:19 -0300

    Feat: dump e load de dados

--- core/models/editora.py
+++ core/models/editora.py
@@ -4,6 +4,8 @@ from django.db import models
 class Editora(models.Model):
     nome = models.CharField(max_length=100, blank=True, null=True)
     site = models.URLField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    cidade = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.nome
