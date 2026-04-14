7d127ad8030174b03afc8714945a3254599df7fa
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2026-04-14 16:32:41 -0300

    feat: customização do Admin

--- core/admin.py
+++ core/admin.py
@@ -6,9 +6,46 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core import models
+from core.models import Autor, Categoria, Editora, Livro, User
 
 
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email')
+    search_fields = ('nome', 'email')
+    list_filter = ('nome',)
+    ordering = ('nome', 'email')
+    list_per_page = 10
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('descricao',)
+    search_fields = ('descricao',)
+    list_filter = ('descricao',)
+    ordering = ('descricao',)
+    list_per_page = 10
+
+
+@admin.register(Editora)
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'cidade')
+    search_fields = ('nome', 'email', 'cidade')
+    list_filter = ('nome', 'email', 'cidade')
+    ordering = ('nome', 'email', 'cidade')
+    list_per_page = 10
+
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'editora', 'categoria')
+    search_fields = ('titulo', 'editora__nome', 'categoria__descricao')
+    list_filter = ('editora', 'categoria')
+    ordering = ('titulo', 'editora', 'categoria')
+    list_per_page = 25
+
+
+@admin.register(User)
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
 
@@ -49,10 +86,3 @@ class UserAdmin(BaseUserAdmin):
             },
         ),
     )
-
-
-admin.site.register(models.Autor)
-admin.site.register(models.Categoria)
-admin.site.register(models.Editora)
-admin.site.register(models.Livro)
-admin.site.register(models.User, UserAdmin)
